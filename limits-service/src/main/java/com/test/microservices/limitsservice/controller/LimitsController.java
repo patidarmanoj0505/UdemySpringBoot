c2d867a30fff420c15664e5d365f53d336b3b0e0
Manoj Patidar <manojpatidar0505@yahoo.com>
2022-05-04 18:26:40 +0530

    Limit controller

--- limits-service/src/main/java/com/test/microservices/limitsservice/controller/LimitsController.java
+++ limits-service/src/main/java/com/test/microservices/limitsservice/controller/LimitsController.java
@@ -1,14 +1,19 @@
 package com.test.microservices.limitsservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.test.microservices.limitsservice.bean.Limits;
+import com.test.microservices.limitsservice.configuration.Configuration;
 
 @RestController
 public class LimitsController {
+	
+	@Autowired
+	private Configuration configuration ;
 	@GetMapping("limits")
 	public Limits retrievLimits() {
-		return new Limits(1, 1000);
+		return new Limits(configuration.getMinimum(), configuration.getMaximum());
 	}
 }
